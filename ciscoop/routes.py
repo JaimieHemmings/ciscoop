d7e382ce29d2cf81ecbb99bed40527afabd31a17
Jaimie <jaimie.j.hemmings@gmail.com>
2024-08-02 14:27:00 +0100

    fix: routes.py pep8 compliance

--- ciscoop/routes.py
+++ ciscoop/routes.py
@@ -5,13 +5,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import pagination
 import re
 
+
 # Index page
 @app.route('/')
 @app.route('/index')
 def home():
     # Get 3 latest blog posts
     latest_posts = Post.query.order_by(Post.created.desc()).limit(3).all()
-    return render_template('index.html', title = "Home", posts = latest_posts)
+    return render_template('index.html', title="Home", posts=latest_posts)
+
 
 # Blog page
 @app.route('/blog')
@@ -19,11 +21,19 @@ def blog():
     # Implement SQLAlchemy pagination for Blog Page
     page = request.args.get('page', 1, type=int)
     # Get current page of posts
-    curr_posts = Post.query.order_by(Post.created.desc()).paginate(page=page, per_page=5)
+    curr_posts = Post.query.order_by(
+        Post.created.desc()).paginate(page=page, per_page=5)
     # Get next and previous page URLs
-    next_url = url_for('blog', page=curr_posts.next_num) if curr_posts.has_next else None
-    prev_url = url_for('blog', page=curr_posts.prev_num) if curr_posts.has_prev else None
-    return render_template('blog.html', title="Blog", posts=curr_posts.items, next_url=next_url, prev_url=prev_url)
+    next_url = url_for(
+        'blog', page=curr_posts.next_num) if curr_posts.has_next else None
+    prev_url = url_for(
+        'blog', page=curr_posts.prev_num) if curr_posts.has_prev else None
+    return render_template(
+        'blog.html', title="Blog",
+        posts=curr_posts.items,
+        next_url=next_url,
+        prev_url=prev_url)
+
 
 # Individual Blog Posts
 @app.route('/blog/<slug>')
@@ -47,7 +57,6 @@ def contact():
         user_id = session['user_id']
         user = User.query.filter_by(id=user_id).first()
         user_email = user.email
-    
     if request.method == 'POST':
         # get form data
         name = request.form['name']
@@ -69,16 +78,21 @@ def contact():
             flash("The title must be longer than 3 characters.")
             return redirect(url_for('contact'))
         if len(message) < 5 or len(message) > 280:
-            flash("Message must be greater than 5 characters and less than 280 characters.")
+            flash(
+                "Message must be greater than 5 "
+                "characters and less than 280 characters.")
             return redirect(url_for('contact'))
         # create new message
-        new_message = Message(name=name, email=email, content=message, title=title)
+        new_message = Message(
+            name=name, email=email, content=message, title=title)
         # add new message to database
         db.session.add(new_message)
         db.session.commit()
         flash("Message sent successfully!")
         return redirect(url_for('contact'))
-    return render_template('contact.html', title="Contact", user_email = user_email)
+    return render_template(
+        'contact.html', title="Contact", user_email=user_email)
+
 
 # Login page
 @app.route('/login', methods=['GET', 'POST'])
@@ -102,6 +116,7 @@ def login():
             return redirect(url_for('login'))
     return render_template('login.html', title="Login")
 
+
 # Register page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -121,7 +136,11 @@ def register():
             flash("Passwords do not match. Please try again.")
             return redirect(url_for('register'))
         # create new user
-        new_user = User(username=username, password=generate_password_hash(password), email=email, role="user")
+        new_user = User(
+            username=username,
+            password=generate_password_hash(password),
+            email=email,
+            role="user")
         # add new user to database
         db.session.add(new_user)
         db.session.commit()
@@ -129,6 +148,7 @@ def register():
         # redirect to login page
         return redirect(url_for('login'))
     return render_template('register.html', title="Register")
+
 
 # Profile page
 @app.route('/profile')
@@ -141,6 +161,7 @@ def profile():
     user_id = session['user_id']
     user = User.query.filter_by(id=user_id).first()
     return render_template('profile.html', title="Profile", user=user)
+
 
 # Admin Page
 @app.route('/profile/admin', methods=['GET', 'POST'])
@@ -173,7 +194,13 @@ def admin():
             flash("Please create a more descriptive preview string")
             return redirect(url_for('admin'))
         # create new post
-        new_post = Post(title=title, content=content, preview=preview, user_id=user_id, slug=slug, user=user)
+        new_post = Post(
+            title=title,
+            content=content,
+            preview=preview,
+            user_id=user_id,
+            slug=slug,
+            user=user)
         # add new post to database
         db.session.add(new_post)
         db.session.commit()
@@ -181,11 +208,14 @@ def admin():
         return redirect(url_for('admin'))
     # Get all blog posts
     all_posts = Post.query.all()
-    
     messageData = Message.query.all()
     newMessages = len(messageData)
+    return render_template(
+        'admin.html',
+        title="Admin",
+        posts=all_posts,
+        newMessages=newMessages)
 
-    return render_template('admin.html', title="Admin", posts=all_posts, newMessages = newMessages)
 
 # Edit posts list
 @app.route('/profile/admin/edit-posts/', methods=['GET', 'POST'])
@@ -204,11 +234,24 @@ def posts_list():
     # Implement SQLAlchemy pagination for Blog Page
     page = request.args.get('page', 1, type=int)
     # Get current page of posts
-    curr_posts = Post.query.order_by(Post.created.desc()).paginate(page=page, per_page=5)
+    curr_posts = Post.query.order_by(
+        Post.created.desc()).paginate(page=page, per_page=5)
     # Get next and previous page URLs
-    next_url = url_for('posts_list', page=curr_posts.next_num) if curr_posts.has_next else None
-    prev_url = url_for('posts_list', page=curr_posts.prev_num) if curr_posts.has_prev else None
-    return render_template('posts-list.html', title="Edit Posts", posts=curr_posts.items, next_url=next_url, prev_url=prev_url)
+    next_url = url_for(
+        'posts_list',
+        page=curr_posts.next_num
+        ) if curr_posts.has_next else None
+    prev_url = url_for(
+        'posts_list',
+        page=curr_posts.prev_num
+        ) if curr_posts.has_prev else None
+    return render_template(
+        'posts-list.html',
+        title="Edit Posts",
+        posts=curr_posts.items,
+        next_url=next_url,
+        prev_url=prev_url)
+
 
 # Edit Post
 @app.route('/profile/admin/edit-post/<int:id>', methods=['GET', 'POST'])
@@ -243,6 +286,7 @@ def edit_post(id):
         return redirect(url_for('posts_list'))
     return render_template('edit-post.html', title="Edit Post", post=post)
 
+
 # Delete Post
 @app.route('/profile/admin/delete-post/<int:id>')
 def delete_post(id):
@@ -265,6 +309,7 @@ def delete_post(id):
     flash("Post deleted successfully!")
     return redirect(url_for('posts_list'))
 
+
 # Messages Page
 @app.route('/admin/messages', methods=['GET', 'POST'])
 def messages_page():
@@ -281,7 +326,11 @@ def messages_page():
         return redirect(url_for('home'))
     # Get all messages
     messageData = Message.query.all()
-    return render_template('messages.html', title="Messages", messages=messageData)
+    return render_template(
+        'messages.html',
+        title="Messages",
+        messages=messageData)
+
 
 # Delete Message
 @app.route('/admin/messages/delete/<int:id>')
@@ -305,6 +354,7 @@ def delete_message(id):
     flash("Message deleted successfully!")
     return redirect(url_for('messages_page'))
 
+
 # Logout
 @app.route('/logout')
 def logout():
@@ -312,12 +362,20 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for('home'))
 
+
 # Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html', title="Uh oh! Error: 404", error_message=e), 404
+    return render_template(
+        '404.html',
+        title="Uh oh! Error: 404",
+        error_message=e), 404
+
 
 # Internal Server Error
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html', title="Uh oh! Error: 500", error_message=e), 500
+    return render_template(
+        '500.html',
+        title="Uh oh! Error: 500",
+        error_message=e), 500
