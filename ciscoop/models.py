08edf875cef1c49256355a6988b5766b72fa8657
Jaimie <jaimie.j.hemmings@gmail.com>
2024-08-02 14:28:54 +0100

    fix: models.py pep8 compliance

--- ciscoop/models.py
+++ ciscoop/models.py
@@ -1,42 +1,46 @@
 from ciscoop import db
 
+
 class User(db.Model):
-  # schema for User model
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  password = db.Column(db.String(200), unique=False, nullable=False)
-  email = db.Column(db.String(50), unique=True, nullable=False)
-  role = db.Column(db.String(20), unique=False, nullable=False, default="user")
-  created = db.Column(db.DateTime, server_default=db.func.now())
+    # schema for User model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    role = db.Column(
+        db.String(20), unique=False, nullable=False, default="user")
+    created = db.Column(db.DateTime, server_default=db.func.now())
 
-  def __repr__(self):
-      # return a string representation of the object
-      return '<User %r>' % self.username
-  
+    def __repr__(self):
+        # return a string representation of the object
+        return '<User %r>' % self.username
+
+
 class Post(db.Model):
-  # schema for Post model
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(80), unique=True, nullable=False)
-  content = db.Column(db.Text, unique=False, nullable=False)
-  preview = db.Column(db.String(50), unique=False, nullable=True)
-  created = db.Column(db.DateTime, server_default=db.func.now())
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-  user = db.relationship('User', backref=db.backref('Post', lazy=True))
-  slug = db.Column(db.String(80), unique=True, nullable=False)
+    # schema for Post model
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    content = db.Column(db.Text, unique=False, nullable=False)
+    preview = db.Column(db.String(50), unique=False, nullable=True)
+    created = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('Post', lazy=True))
+    slug = db.Column(db.String(80), unique=True, nullable=False)
 
-  def __repr__(self):
-      # return a string representation of the object
-      return '<Post %r>' % self.title
-  
+    def __repr__(self):
+        # return a string representation of the object
+        return '<Post %r>' % self.title
+
+
 class Message(db.Model):
-  # schema for Message model
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), unique=False, nullable=False)
-  email = db.Column(db.String(120), unique=False, nullable=False)
-  title = db.Column(db.String(80), unique=False, nullable=False)
-  content = db.Column(db.String(280), unique=False, nullable=False)
-  created = db.Column(db.DateTime, server_default=db.func.now())
+    # schema for Message model
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    title = db.Column(db.String(80), unique=False, nullable=False)
+    content = db.Column(db.String(280), unique=False, nullable=False)
+    created = db.Column(db.DateTime, server_default=db.func.now())
 
-  def __repr__(self):
-      # return a string representation of the object
-      return '<Message %r>' % self.title
+    def __repr__(self):
+        # return a string representation of the object
+        return '<Message %r>' % self.title
